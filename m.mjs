6ef7b0cd7b49f26faea719f884df1857b1cee6a3
Hhimanshhu <149136502+Hhimanshhu@users.noreply.github.com>
2025-03-14 11:43:20 +0530

    Add files via upload

---
+++ m.mjs
@@ -0,0 +1,5 @@
+import asd, {b,c,d} from './m1.mjs'
+console.log(asd);
+console.log(b);
+console.log(c);
+console.log(d);
---
+++ m1.mjs
@@ -0,0 +1,9 @@
+let a = 5;
+let b = "HImanshu";
+let c = "Magar";
+let d = 20;
+
+export default a;
+export {b};
+export {c};
+export {d};
